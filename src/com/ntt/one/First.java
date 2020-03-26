b5d634ab15ccd5904abc11dc589a7ea3af15831d
prachi shukla <prachishuklaaec@gmail.com>
2020-03-26 18:26:55 +0530

    first commit

---
+++ src/com/ntt/one/First.java
@@ -0,0 +1,14 @@
+package com.ntt.one;
+
+public class First {
+		  public static void main(String[] args) {
+		    int time = 20;
+		    if (time < 18) {
+		      System.out.println("Good day.");
+		    } else {
+		      System.out.println("Good evening.");
+		    }  
+		  }
+		}
+
+
